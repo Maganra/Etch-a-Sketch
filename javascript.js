cc58fe963929816b2cf08ea3d3a76a7421e36630
Maganra <brennan.l.olson@gmail.com>
2022-07-18 13:31:52 -0500

    overhauled previous method for creating the divs and added ui elements for grid size customization and clearing the grid

--- javascript.js
+++ javascript.js
@@ -1,14 +1,52 @@
-let i = 0
-let n = 16;
+const grid = document.querySelector('.grid');
+let gridSize = 16;
+const resetBtn = document.querySelector('.reset');
+const applyGridSize = document.querySelector('.apply');
+let squareSize = 8;
 
-container.innerHTML = `<div class="row">${'<div class="cell"></div>'.repeat(n)}</div>`.repeat(n);
+createGrid(squareSize);
 
-let item = document.getElementById("container");
-item.addEventListener("mouseover", function(event) {
-    event.target.style.background = "red";
-})
+function createSquare(size) {
+    const div = document.createElement('div');
+    div.classList.add('box');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    return div;
+}
 
-// item.addEventListener("mouseout", function(event) {
+function gridDimension() {
+    squareSize = prompt("Enter size of grid: ");
+    if (squareSize > 100 || squareSize < 1 || squareSize == null || squareSize == "" || isNaN(squareSize)) {
+        alert("Please enter a number between 1 and 100");
+        gridDimension();
+    }
+    return squareSize;
+}
 
-//     event.target.style.background = "";
-// })
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            grid.appendChild(createSquare(grid.clientWidth / gridSize));
+        }
+    }
+}
+
+function reset() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    }
+    createGrid(squareSize);
+}
+
+grid.addEventListener('mouseover', function (e) {
+    if (e.target.matches('.box')) {
+        e.target.classList.add('active');
+    }
+});
+
+applyGridSize.addEventListener('click', function () {
+    gridDimension();
+    reset();
+});
+
+resetBtn.addEventListener('click', reset);
