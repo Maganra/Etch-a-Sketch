3f512aed9c43b27d60a0ceac2414b5c500e1c5f2
Maganra <brennan.l.olson@gmail.com>
2022-07-18 15:26:44 -0500

    added ui and started on js for shading tool

--- javascript.js
+++ javascript.js
@@ -3,8 +3,10 @@ let gridSize = 16;
 const resetBtn = document.querySelector('.reset');
 const applyGridSize = document.querySelector('.apply');
 const randomColor = document.querySelector('.random');
+// const shadeColor = document.querySelector('.shade');
 let squareSize = 8;
 let setRandom = false;
+let setShade = false;
 
 createGrid(squareSize);
 
@@ -46,14 +48,32 @@ randomColor.addEventListener('click', function () {
         randomColor.style.backgroundColor = 'var(--color-button)';
     } else {
         setRandom = true;
+        // setShade = false;
+        // shadeColor.style.backgroundColor = 'var(--color-button)';
         randomColor.style.backgroundColor = 'var(--color-button-hover)';
     }
 });
+
+// shadeColor.addEventListener('click', function () { 
+//     if (setShade) {
+//         setShade = false;
+//         shadeColor.style.backgroundColor = 'var(--color-button)';
+//     } else {
+//         setShade = true; 
+//         setRandom = false;
+//         randomColor.style.backgroundColor = 'var(--color-button)';
+//         shadeColor.style.backgroundColor = 'var(--color-button-hover)';
+//     }
+// });
 
 grid.addEventListener('mouseover', function (e) {
     if (e.target.matches('.box')) {
         if (setRandom) {
             e.target.style.backgroundColor = setColor();
+        // } 
+        // else if (setShade) {
+        //     let darken = e.target.style.backgroundColor;
+        //     shade(darken);
         } else {
             e.target.classList.add('active');
         }
@@ -73,3 +93,9 @@ function setColor() {
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     return "#" + randomColor;
 }
+
+// function shade(darken) {
+//     let shader = darken.replace(/[^,]+(?=\))/, '0.5');
+//     console.log(shader);
+//     return shader;
+// }
